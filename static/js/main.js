bacaf087ff1ef9b334c4ff39e4a2a4c5c076806a
Checksum <collier.bell20@gmail.com>
2021-06-02 23:03:27 -0500

    disable the sticky header for now

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,14 @@
 // sticky header
-$(window).scroll(() => {
-    var header = document.getElementById("navbar");
-    var sticky = header.offsetTop;
+// $(window).scroll(() => {
+//     var header = document.getElementById("navbar");
+//     var sticky = header.offsetTop;
 
-    if (window.pageYOffset > sticky) {
-        header.classList.add("minimized");
-    } else {
-        header.classList.remove("minimized");
-    }
-});
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("minimized");
+//     } else {
+//         header.classList.remove("minimized");
+//     }
+// });
 
 //toggle navbar for mobile
 function togglenavbar() {
