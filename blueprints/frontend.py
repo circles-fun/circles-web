00069cf97a86189d6496eec740bb45c87c052700
Checksum <collierdevs@tuta.io>
2021-06-30 21:48:50 -0500

    aaaaaAAA

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -74,7 +74,7 @@ async def discord_callback():
             if not respa or respa.status != 200:
                 return await flash('error', "Failed to get your Discord OAuth token. (Malformed URL?)", "settings/profile")
 
-        token = await respa.json()
+        token = await respa.text()
 
         log(f"{token}", Ansi.GREEN)
         
