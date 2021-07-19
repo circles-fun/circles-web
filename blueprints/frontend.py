25561fdb6e8cd316bb2b3d451574d8c11c95a304
Checksum <collierdevs@tuta.io>
2021-07-19 10:54:16 +0200

    fix site registration with new stats table.

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -548,21 +548,23 @@ async def register_post():
     else:
         country = 'xx'
 
-    # add to `users` table.
-    user_id = await glob.db.execute(
-        'INSERT INTO users '
-        '(name, safe_name, email, pw_bcrypt, country, creation_time, latest_activity) '
-        'VALUES (%s, %s, %s, %s, %s, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())',
-        [username, safe_name, email, pw_bcrypt, country]
-    )
+    async with glob.db.pool.acquire() as conn:
+        async with conn.cursor() as db_cursor:
+            # add to `users` table.
+            await db_cursor.execute(
+                'INSERT INTO users '
+                '(name, safe_name, email, pw_bcrypt, country, creation_time, latest_activity) '
+                'VALUES (%s, %s, %s, %s, %s, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())',
+                [username, safe_name, email, pw_bcrypt, country]
+            )
+            user_id = db_cursor.lastrowid
 
-    # add to `stats` table.
-    await glob.db.execute(
-        'INSERT INTO stats '
-        '(id) VALUES (%s)',
-        [user_id]
-    )
-    # (end of lock)
+            # add to `stats` table.
+            await db_cursor.executemany(
+                'INSERT INTO stats '
+                '(id, mode) VALUES (%s, %s)',
+                [(user_id, mode) for mode in range(8)]
+            )
 
     if glob.config.debug:
         log(f'{username} has registered - awaiting verification.', Ansi.LGREEN)
